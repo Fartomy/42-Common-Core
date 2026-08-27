cdf722894e8de24909f12fc86efba55628e68f31
Fartomy <58911876+Fartomy@users.noreply.github.com>
2026-08-27 19:36:09 +0300

    Delete python_modules directory

--- python_modules/module_00/ex4/ft_plant_age.py
+++
@@ -1,4 +0,0 @@
-def ft_plant_age() -> None:
-    age: int = int(input("Enter plant age in days: "))
-    print("Plant is ready to harvest!" if age > 60
-          else "Plant needs more time to grow.")
--- python_modules/module_00/ex5/ft_water_reminder.py
+++
@@ -1,3 +0,0 @@
-def ft_water_reminder() -> None:
-    d: int = int(input("Days since last watering: "))
-    print("Water the plants!" if d > 2 else "Plants are fine")
